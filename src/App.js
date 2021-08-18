2341cbfcf6b4260035d1b0404eb2e4a45b554902
HOTJAE <gouz7514@naver.com>
2021-08-18 21:57:59 +0900

    Update : props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <h1>Hello World!</h1>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
-const Header = () => {
+const Header = ({ title }) => {
   return (
     <div>
       <header>
-        <h1>Task Tracker</h1>
+        <h1>{title}</h1>
       </header>
     </div>
   )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker'
 }
 
 export default Header

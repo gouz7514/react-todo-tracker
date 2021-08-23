38e842c11ea0130f5214dd959b8faf3b19de5807
gouz7514 <gouz7514@gmail.com>
2021-08-23 22:04:58 +0900

    Update : add task on server

--- src/App.js
+++ src/App.js
@@ -27,11 +27,30 @@ const App = () => {
   }
 
   // Add Task
-  const addTask = (task) => {
+  const addTask = async (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
-    
-    setTasks([...tasks, newTask])
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
+
+    // axios
+    const data = await axios.post('http://localhost:5000/tasks', {
+      'id': id,
+      ...task
+    })
+
+    setTasks([...tasks, data.data])
+
+    // fetch
+    // const res = await fetch('http://localhost:5000/tasks', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-type': 'application/json'
+    //   },
+    //   body: JSON.stringify(task)
+    // })
+    // const data = await res.json()
+
+    // setTasks([...tasks, data])
   }
 
   // Delete Task
